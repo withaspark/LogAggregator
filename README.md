2e0f62e2b21941d00f623fadc7a67722e8072061
Stephen Parker <web@withaspark.com>
2019-08-19 00:38:53 -0400

    Add analyze command to store logs in local sqlite database

--- README.md
+++ README.md
@@ -19,8 +19,16 @@
 
 #### To build index
 
+The `pull` command fetches all log files configured to be watched and builds a
+master log index containing all records.
+
+**NOTE:** Deduplication is in place to eliminate duplicate records being indexed
+with each run, but will also eliminate identical log messages that occurred on
+the same host, log file, and with the same timestamp (if present).
+
+To run,
 ```sh
-path/to/pull <local working directory>
+path/to/pull <local storage directory>
 ```
 
 For example,
@@ -30,6 +38,15 @@ For example,
 
 #### To query index
 
+The `query` command will search the master log index for all records matching.
+Simple search terms like "Page not found" as well as regular expressions are
+supported.
+
+Each log message returned from the index will contain the hostname and log file
+name prepended to the line. This allows filtering by hostname and log file name
+in addition to the search pattern.
+
+To run,
 ```sh
 path/to/search "<search regex>"
 ```
@@ -39,8 +56,43 @@ For example,
 ./search "host\.example\.com.*auth\.log.*Failed password"
 ```
 
+#### To parse and store in local SQLite database
+
+The `analyze` command parses the master log index and stores messages to a local
+SQLite database for easier integration for other use-cases and extensions.
+
+**NOTE:** This feature requires the PHP pdo-sqlite extension be installed.
+
+The database and tables will automatically be created, as needed, and saved to
+`./database/database.sqlite`.
+
+Log messages are saved to the `logs` table. Available columns include:
+
+| Name | Datatype | Description |
+|---|---|---|
+|`id`|`INTEGER AUTOINCREMENT`|Unique ID of the row. For future use.|
+|`hash`|`TEXT`|The SHA-1 hash of the original log message. Used for deduplication.|
+|`host`|`TEXT`|The full hostname used to make the connection to the host when pulling the logs.|
+|`file`|`TEXT`|The name of the log file the message was found in.|
+|`message`|`TEXT`|The full log message.|
+|`created_at`|`DATETIME`|The datetime the message was first seen by the `analyze` command.|
+
+To run,
+```sh
+path/to/analyze
+```
+
+For example,
+```sh
+./analyze
+```
+
 #### To tail logs
 
+The `tail` command can be used to simultaneously tail all of the configured
+remote logs.
+
+To run,
 ```sh
 path/to/tail
 ```
@@ -54,7 +106,10 @@ Send SIGINT `<ctrl>`+`c` to exit.
 
 ## Roadmap
 
-* Accept config file as commandline argument. This would allow separate config files for different scenarios and commands.
+* Accept config file as commandline argument. This would allow separate config
+  files for different scenarios and commands. In lieu of this feature, we may
+  add support for a command-line option to scope by host, log group, and/or log
+  file.
 * Config file validation to minimize unhelpful error messages.
 * Handle losses of connectivity to remote hosts.
 * Add default log groups that require only hosts, e.g., syslog, apache, nginx, etc.
--- src/withaspark/LogAggregator/helpers.php
+++ src/withaspark/LogAggregator/helpers.php
@@ -2,10 +2,19 @@
 
 namespace withaspark\LogAggregator;
 
+use PDO;
+use PDOException;
+use PDOStatement;
 use withaspark\LogAggregator\Host;
 use withaspark\LogAggregator\Group;
 
 if (! function_exists('withaspark\LogAggregator\parse')) {
+    /**
+     * Parse the configuration file.
+     *
+     * @param string $config Path to the configuration file
+     * @return [\withaspark\LogAggregator\Host[], \withaspark\LogAggregator\Group[]]
+     */
     function parse(string $config = 'config.json'): array
     {
         $config = json_decode(file_get_contents('config.json'), false);
@@ -30,7 +39,6 @@ if (! function_exists('withaspark\LogAggregator\parse')) {
                 }
             }
 
-            // TODO: If already exists, append hosts and logs instead of overwriting
             if (! array_key_exists($group->name, $groups)) {
                 $groups[$group->name] = new Group($hs, $group->logs);
                 continue;
@@ -46,6 +54,13 @@ if (! function_exists('withaspark\LogAggregator\parse')) {
 }
 
 if (! function_exists('withaspark\LogAggregator\pull')) {
+    /**
+     * Echo the shell commands required to make a local copy of all logs being
+     * watched. These commands must be executed in a shell.
+     *
+     * @param string $output_dir Path to the directory where logs will be kept
+     * @return void
+     */
     function pull(string $output_dir = '/home/logging/logs'): void
     {
         list($hosts, $groups) = parse('config.php');
@@ -59,7 +74,6 @@ if (! function_exists('withaspark\LogAggregator\pull')) {
         $map = Group::getMap();
         $count = 0;
         $total = count($map, COUNT_RECURSIVE) - count($map);
-        $datestamp = date('Ymd_His');
 
         echo sprintf('WASLAP_START=%d', time()) . "\n";
         echo sprintf("echo \"-- %s --\n\"", date('Y-m-d H:i:s')) . "\n";
@@ -108,13 +122,76 @@ if (! function_exists('withaspark\LogAggregator\pull')) {
     }
 }
 
+if (! function_exists('withaspark\LogAggregator\analyze')) {
+    /**
+     * Creates a local database of all messages that have been pulled.
+     *
+     * @return void
+     */
+    function analyze(): void
+    {
+        $total = 0;
+        $count = 0;
+        $lines = [];
+        $last_percentage = 0;
+        $fh = fopen('master.log', 'r');
+
+        fprintf(STDOUT, "Analyzing pulled logs ...\n");
+        while (! feof($fh)) {
+            $total++;
+            fgets($fh);
+        }
+
+        rewind($fh);
+        while (! feof($fh)) {
+            $count++;
+            $percentage = floor(($count - 1) / $total * 100);
+
+            if ($percentage != $last_percentage) {
+                fprintf(
+                    STDOUT,
+                    "[%s%%] Building log file database ...\r",
+                    str_pad(number_format($percentage, 0), 3, ' ', STR_PAD_LEFT)
+                );
+
+                $last_percentage = $percentage;
+            }
+
+            $line = fgets($fh);
+
+            if ($line) {
+                $lines[sha1($line)] = $line;
+            }
+
+            // Maximum of 1000 variables and 1 mil characters per query
+            if (count($lines) >= 100 || $line === false) {
+                // Relying on unique index for performance
+                // $lines = array_intersect_key($lines, array_flip(findNewMessages(array_keys($lines))));
+
+                if (count($lines) > 0) {
+                    insertLogMessages($lines);
+                }
+                $lines = [];
+            }
+        }
+        fclose($fh);
+
+        fprintf(STDOUT, "[%s%%] Log file database successfully built.\n", 100);
+    }
+}
+
 if (! function_exists('withaspark\LogAggregator\tail')) {
+    /**
+     * Echo the shell commands required to tail  all of te logs being watched.
+     * These commands must be executed in a shell.
+     *
+     * @return void
+     */
     function tail(): void
     {
         list($hosts, $groups) = parse('config.php');
 
         $map = Group::getMap();
-        $datestamp = date('Ymd_His');
 
         echo sprintf("echo -- %s --\n", date('Y-m-d H:i:s')) . "\n";
 
@@ -122,8 +199,175 @@ if (! function_exists('withaspark\LogAggregator\tail')) {
             $host = $hosts[$h];
 
             foreach ($logs as $log) {
-                echo sprintf('ssh -p %d %s@%s tail -n1 -f %s &', $host->port, escapeshellarg($host->user), escapeshellarg($host->host), escapeshellarg($log)) . "\n";
+                echo sprintf(
+                    'ssh -p %d %s@%s tail -n0 -f %s &',
+                    $host->port,
+                    escapeshellarg($host->user),
+                    escapeshellarg($host->host),
+                    escapeshellarg($log)
+                ) . "\n";
             }
         }
+    }
+}
+
+if (! function_exists('withaspark\LogAggregator\initdb')) {
+    /**
+     * Establish a connection to the local log database. If one does not exist,
+     * create it.
+     *
+     * @return \PDO
+     */
+    function initdb(): PDO
+    {
+        static $connection = null;
+
+        if (is_null($connection)) {
+            $db = 'database/database.sqlite';
+
+            if (! file_exists($db)) {
+                touch($db);
+            }
+
+            if (! is_writable($db)) {
+                fprintf(STDERR, "\nERROR: Unable to write to analysis database %s\n\nCreate or make writeable by current user.\n", $db);
+                exit(1);
+            }
+
+            $connection = new PDO(sprintf("sqlite:%s", $db));
+
+            $results = $connection->query('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'logs\';');
+
+            if (count($results->fetchAll()) == 0) {
+                createLogsTable($connection);
+            }
+        }
+
+        return $connection;
+    }
+}
+
+if (! function_exists('withaspark\LogAggregator\createLogsTable')) {
+    /**
+     * Create the logs table in the local log database.
+     *
+     * @param \PDO $connection PDO connection to local log database
+     * @return void
+     */
+    function createLogsTable(PDO $connection): void
+    {
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        try {
+            $statement = $connection->exec("
+                CREATE TABLE logs (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    hash TEXT NOT NULL UNIQUE,
+                    host TEXT NOT NULL,
+                    file TEXT NOT NULL,
+                    message TEXT NOT NULL,
+                    created_at DATETIME NOT NULL
+                );
+                CREATE INDEX logs_hash_index ON logs (hash);
+                CREATE INDEX logs_host_index ON logs (host);
+                CREATE INDEX logs_file_index ON logs (file);
+                CREATE INDEX logs_created_at_index ON logs (created_at);
+            ");
+        } catch (PDOException $e) {
+            fprintf(STDERR, "\nERROR: Unable to create to analysis database %s.\n", $e->getMessage());
+            exit(1);
+        }
+    }
+}
+
+if (! function_exists('withaspark\LogAggregator\findNewMessages')) {
+    /**
+     * Find which log message hashes are not found in the local log database.
+     *
+     * @param string[] $hashes Raw log message hashes to search for
+     * @return string[]
+     */
+    function findNewMessages(array $hashes): array
+    {
+        $select = implode(', ', array_fill(0, count($hashes), '?'));
+        $connection = initdb();
+        $query = sprintf('SELECT `hash` FROM logs WHERE `hash` IN (%s);', $select);
+
+        $statement = $connection->prepare($query);
+        foreach ($hashes as $j => $hash) {
+            $statement->bindValue(($j + 1), $hash);
+        }
+        $statement->execute();
+
+        $found = array_column($statement->fetchAll(), 'hash');
+
+        $hashes = array_diff($hashes, $found);
+
+        return $hashes;
+    }
+}
+
+if (! function_exists('withaspark\LogAggregator\insertLogMessages')) {
+    /**
+     * Insert the messages into the local log database with key properties
+     * extracted and saved.
+     *
+     * @param string[] $raws Array of raw log messages indexed by hash
+     * @return boolean
+     */
+    function insertLogMessages(array $raws): bool
+    {
+        static $hosts = null;
+
+        if (is_null($hosts)) {
+            $hosts = array_keys(parse('config.php')[0]);
+        }
+
+        $inserts = [];
+        $time = date('Y-m-d H:i:s');
+
+        foreach ($raws as $hash => $raw) {
+            $result = explode(':', $raw, 2);
+
+            if (count($result) !== 2) {
+                return false;
+            }
+
+            list($preamble, $message) = $result;
+
+            $matches = [];
+            preg_match(sprintf('/^(.*)(%s)(.*)$/', str_replace('.', '\.', implode('|', $hosts))), $preamble, $matches);
+
+            if (count($matches) !== 4) {
+                return false;
+            }
+
+            list(, , $host, $file) = $matches;
+
+            $inserts[] = (object) [
+                'hash' => $hash,
+                'host' => $host,
+                'file' => $file,
+                'message' => $message,
+            ];
+        }
+        unset($raws);
+
+        $insert = implode(',', array_fill(0, count($inserts), '(?, ?, ?, ?, ?)'));
+
+        $connection = initdb();
+
+        $statement = $connection->prepare(sprintf('INSERT INTO logs (`hash`, `host`, `file`, `message`, `created_at`) VALUES %s;', $insert));
+        foreach ($inserts as $j => $ins) {
+            $statement->bindValue((5 * $j) + 1, $ins->hash);
+            $statement->bindValue((5 * $j) + 2, $ins->host);
+            $statement->bindValue((5 * $j) + 3, $ins->file);
+            $statement->bindValue((5 * $j) + 4, $ins->message);
+            $statement->bindValue((5 * $j) + 5, $time);
+        }
+
+        $statement->execute();
+
+        return true;
     }
 }
